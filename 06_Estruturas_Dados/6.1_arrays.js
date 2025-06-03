cca2df49706fa05f9656290c7fceb88381fe595c
FranciscoDougllas <franciscodouglas77@outlook.com>
2025-06-03 15:05:37 -0300

    -------

---
+++ 06_Estruturas_Dados/6.1_arrays.js
@@ -0,0 +1,11 @@
+//Arrays em JavaScript
+
+//Declaração de um array
+let frutas = ['maçã', 'banana', 'laranja'];
+
+//Acessando elementos do array
+console.log(frutas[0]); // 'maçã'
+console.log(frutas[1]); // 'banana'
+
+//Tamanho do array
+console.log(frutas.length); // 3
